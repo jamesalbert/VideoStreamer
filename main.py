c629cb77df0f547d171f9f68fc27a9d742029a64
jamesalbert <james.albert72@gmail.com>
2015-06-29 22:07:10 -0700

    added php uploader (uploadify)

--- main.py
+++ main.py
@@ -134,4 +134,4 @@ def listings():
     return render_template('listings.html', query=query, logged_in=hasattr(user, 'email'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, threaded=True)
+    app.run(port=5000, threaded=True)
---
+++ static/uploadify/uploadify.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Uploadify
+Copyright (c) 2012 Reactive Apps, Ronnie Garcia
+Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
+*/
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+echo exec('whoami'), "\n";
+// Define a destination
+$targetFolder = '/upload'; // Relative to the root
+#echo $_POST['token'];
+$verifyToken = md5('unique_salt' . $_POST['timestamp']);
+if (!empty($_FILES) && $_POST['token'] == $verifyToken) {
+	$tempFile = $_FILES['Filedata']['tmp_name'];
+	$targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
+	$targetFile = rtrim($targetPath,'/') . '/' . $_FILES['Filedata']['name'];
+	// Validate the file type
+	$fileTypes = array('mp4'); // File extensions
+	$fileParts = pathinfo($_FILES['Filedata']['name']);
+	echo $targetFile, "\n";
+	if (in_array($fileParts['extension'],$fileTypes)) {
+		echo move_uploaded_file($tempFile,$targetFile), "\n";
+		echo '1', "\n";
+	} else {
+		echo 'Invalid file type. \n';
+	}
+}
+echo 'post finished';
+?>
